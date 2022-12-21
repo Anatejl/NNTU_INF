c8c33a0fc83d9de16c263d86798c8f71f58542be
Anatejl <laolxs@gmail.com>
2022-12-21 22:50:25 +0300

    until 10s

---
+++ 1/Tinakov/2.1/2.1.c
@@ -0,0 +1,45 @@
+//
+// Created by Anatejl on 21.12.2022.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    double a, b, c, d, k, min, max;
+
+    system("chcp 1251 > nul"); // Строка дублирует ввод в консоли.
+
+    printf("Введите последовательность (a, b, c, d, k): ");
+    scanf_s("%lf", &a);
+    scanf_s("%lf", &b);
+    scanf_s("%lf", &c);
+    scanf_s("%lf", &d);
+    scanf_s("%lf", &k);
+
+    if (c > d && c > k) {
+        max = c;
+    } else if (c < d && d > k) {
+        max = d;
+    } else{
+        max = k;
+    }
+
+    if (a < b) {
+         min = a;
+    } else {
+        min = b;
+    }
+
+    if (min < max) {
+        printf("%.1f\n", max);
+    } else {
+        printf("%.1f\n", min);
+    }
+
+    //printf("\n %lf %lf \n", min, max);
+
+    system("pause");
+
+    return 0;
+}
