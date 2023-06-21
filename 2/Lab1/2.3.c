62f44eccaf660f40dfe6f95d2c9738948521ad60
Anatejl <laolxs@gmail.com>
2023-06-21 23:55:52 +0300

    monkaKMS 2.4 3-5

--- 2/Lab1/2.3.c
+++ 2/Lab1/2.3.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
 
+	int iteration = 1;
 
+	for (int value = 1; value < 100000; value *= 2) {
+		printf("#%d - %d\n", iteration, value);
+		iteration++;
+	}
 
 	return 0;
 }
---
+++ 2/Lab1/2.4.c
@@ -0,0 +1,43 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+	
+	int inputNumber = 0;
+
+	while (1) {
+
+		int temp = 0;
+
+		printf("Input an integer:");
+		scanf_s("%d", &inputNumber);
+		
+		if (inputNumber % 3 == 0) {
+			printf("Number remain the same - %d\n", inputNumber);
+		}
+
+		else if (inputNumber % 5 == 0) {
+			
+			if (inputNumber % 3 == 0) {
+
+				// Умные мысли приследовали автора, но он был быстрее
+
+			}
+			
+			else {
+				temp = pow(inputNumber, 2);
+				printf("Number been raised by the power of 2 - %d\n", temp);
+			}
+		}
+		
+		else if(inputNumber % 7 ==0){
+			printf("Success!\n");
+			break;
+		}
+		else {
+			printf("Try again.\n");
+		}
+	}
+
+	return 0;
+}
