c9e87e2cb1ddd64201f2efe6417cf42d9a574f60
john@WSL <laolxs@gmail.com>
2024-10-25 00:00:50 +0300

    DID ONE EBAT YA BOG

--- 1/Egorov/1.c
+++ 1/Egorov/1.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "1.h"
 
-typedef struct Variables{
-
-    int n, d, a1;
-
-}Variables;
-
-bool get_input(Variables var){
+void get_input(Variables *p_var){
 
     printf("Input n: ");
-    scanf("%d", var.n);
+    scanf("%lf", &p_var->n);
 
     printf("Input a1: ");
-    scanf("%d", var.a1);
+    scanf("%lf", &p_var->a1);
 
+    printf("Input d (sequence diviation): ");
+    scanf("%lf", &p_var->d);
 
-    return true;
+}
+
+void do_process(Variables* p_var){
+
+    p_var->summ = p_var->n/2*(2*p_var->a1+(p_var->n-1)*p_var->d);
+
+}
+
+void do_output(Variables *p_var){
+
+    printf("Summ is: %.f\n", p_var->summ);
+
 }
 
 int main(){
 
     Variables var;
 
-    get_input(var);
+    get_input(&var);
+    do_process(&var);
+    do_output(&var);
 
     return 0;
 
---
+++ 1/Egorov/1.h
@@ -0,0 +1,15 @@
+#ifndef NNTU_1
+#define NNTU_1
+
+typedef struct Variables{
+
+    double n, d, a1;
+    double summ;
+
+}Variables;
+
+void get_input(Variables *var);
+void do_process(Variables *var);
+void do_output(Variables *var);
+
+#endif //NNTU_1
