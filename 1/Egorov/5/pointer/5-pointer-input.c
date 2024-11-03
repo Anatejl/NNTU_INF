54b196e537bf3fab02a59a9c997b13869c65fa66
Alexandr <laolxs@gmail.com>
2024-11-03 13:39:58 +0300

    I AM A GOD

--- 1/Egorov/5/pointer/5-pointer-input.c
+++ 1/Egorov/5/pointer/5-pointer-input.c
@@ -1,10 +1,11 @@
 #include "5-pointer-header.h"
 
 void do_input(Personnel* employees, Aux* aux){
-
-    //aux->known_departments[DEPT_COUNT] = 101, 102, 103, 104;
-    aux->known_departments[0] = 101;
-    aux->department_counts[DEPT_COUNT] = 0, 0, 0, 0;
+    
+    for (int i = 0; i < DEPT_COUNT; i++) {
+        aux->known_departments[i] = 101 + i;
+        aux->department_counts[i] = 0;
+    }
 
     for (int i = 0; i < HR_COUNT; ++i){
 
