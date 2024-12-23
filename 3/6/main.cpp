dca4d5550387425eb953493a9ccac47eb4d3f016
john@WSL <laolxs@gmail.com>
2024-12-23 12:19:30 +0300

    six

--- 3/6/main.cpp
+++ 3/6/main.cpp
@@ -1,8 +1,7 @@
 #include "application.h"
 
 int main(){
-    
-    
+
     std::cout << "Array shortener program!" << std::endl;
     Application app; // Ввод переменной по типу АТД
     int ret = app_run(app); // Запуск приложения
