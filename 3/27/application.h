49ecb40a25fac7fdea93eabc585eeb73ecb971e6
john@WSL <laolxs@gmail.com>
2025-01-11 16:14:27 +0300

    chto

--- 3/27/application.h
+++ 3/27/application.h
@@ -4,21 +4,26 @@
 #include <iostream>
 #include "cartesian.h"
 
-typedef struct Application{
+typedef struct Application_mainline{
 
-    std::vector<std::pair<double, double>> array;
     std::pair<std::pair<double, double>, std::pair<double, double>> distanced;
     //distanced.first - dot 1
     //distanced.second - dot 2 
     //distanced.first.first/second - x/y of a 1nd dot
     //distanced.second.first/second - x/y of a 2nd dot
 
-}Application;
+}Application_mainline;
 
-int app_run(Application& app);
-bool app_start(Application& app);
-bool app_process(Application &app);
-bool app_end(Application& app);
+typedef struct Application_source{
+
+    std::vector<std::pair<double, double>> array;
+
+}Application_source;
+
+int app_run(Application_mainline& app_main, Application_source& app_source);
+bool app_start(Application_mainline& app_main, Application_source& app_source);
+bool app_process(Application_mainline &app_main, Application_source& app_source);
+bool app_end(Application_mainline& app_main, Application_source& app_source);
 
 
 #endif //NNTU_APPLICATION_H
--- 3/27/main.cpp
+++ 3/27/main.cpp
@@ -3,7 +3,8 @@
 
 int main (){
     std::cout << "Cartesian dot finder!" << std::endl;
-    Application app;
-    int ret = app_run(app);
+    Application_mainline app_main;
+    Application_source app_source;
+    int ret = app_run(app_main, app_source);
     return 0;
 }
