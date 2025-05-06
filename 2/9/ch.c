6dbd960f52d9e4f11c6492b1cbd5793f000fa526
John@Mac <laolxs@gmail.com>
2025-05-06 23:01:45 +0300

    Refactor code structure for improved readability and maintainability

--- 2/9/ch.c
+++ 2/9/ch.c
@@ -9,25 +9,25 @@ typedef struct {
     char title[MAX_TITLE_LENGTH];
     int year;
     char genre[MAX_GENRE_LENGTH];
-} Library;
+} Book;
 
-void do_fill(Library catalog[], int *total_publications) {
+void do_fill(Book library[], int *total_publications) {
     printf("Enter the number of publications: ");
     scanf("%d", total_publications);
     getchar();
     
     for (int i = 0; i < *total_publications; i++) {
         printf("Enter title for publication %d: \n", i + 1);
-        fgets(catalog[i].title, MAX_TITLE_LENGTH, stdin);
-        catalog[i].title[strcspn(catalog[i].title, "\n")] = 0;
+        fgets(library[i].title, MAX_TITLE_LENGTH, stdin);
+        library[i].title[strcspn(library[i].title, "\n")] = 0;
 
         printf("Enter year for publication %d: \n", i + 1);
-        scanf("%d", &catalog[i].year);
+        scanf("%d", &library[i].year);
         getchar();
 
         printf("Enter genre for publication %d: \n", i + 1);
-        fgets(catalog[i].genre, MAX_GENRE_LENGTH, stdin);
-        catalog[i].genre[strcspn(catalog[i].genre, "\n")] = 0;
+        fgets(library[i].genre, MAX_GENRE_LENGTH, stdin);
+        library[i].genre[strcspn(library[i].genre, "\n")] = 0;
     }
 }
 
@@ -42,18 +42,18 @@ void do_input(char *search_genre, int *search_year_start, int *search_year_end) 
     scanf("%d", search_year_end);
 }
 
-int do_search(Library catalog[], int catalog_size, const char *genre, int year_start, int year_end, Library matches[]) {
+int do_search(Book library[], int library_size, const char *genre, int year_start, int year_end, Book matches[]) {
     int match_count = 0;
-    for (int i = 0; i < catalog_size; i++) {
-        if ((strcmp(catalog[i].genre, genre) == 0) &&
-        (catalog[i].year >= year_start && catalog[i].year <= year_end)) {
-            matches[match_count++] = catalog[i];
+    for (int i = 0; i < library_size; i++) {
+        if ((strcmp(library[i].genre, genre) == 0) &&
+        (library[i].year >= year_start && library[i].year <= year_end)) {
+            matches[match_count++] = library[i];
         }
     }
     return match_count;
 }
 
-void do_output(Library matches[], int match_count, const char *genre, int year_start, int year_end) {
+void do_output(Book matches[], int match_count, const char *genre, int year_start, int year_end) {
     printf("Titles in genre \"%s\" from %d to %d:\n", genre, year_start, year_end);
     if (match_count == 0) {
         printf("No matching publications found.\n");
@@ -66,16 +66,16 @@ void do_output(Library matches[], int match_count, const char *genre, int year_s
 
 int main() {
 
-    Library catalog[MAX_PUBLICATIONS];
+    Book library[MAX_PUBLICATIONS];
     int total_publications;
-    do_fill(catalog, &total_publications);
+    do_fill(library, &total_publications);
 
     char search_genre[MAX_GENRE_LENGTH];
     int search_year_start, search_year_end;
     do_input(search_genre, &search_year_start, &search_year_end);
 
-    Library matches[MAX_PUBLICATIONS];
-    int match_count = do_search(catalog, total_publications, search_genre, search_year_start, search_year_end, matches);
+    Book matches[MAX_PUBLICATIONS];
+    int match_count = do_search(library, total_publications, search_genre, search_year_start, search_year_end, matches);
     do_output(matches, match_count, search_genre, search_year_start, search_year_end);
 
     return 0;
